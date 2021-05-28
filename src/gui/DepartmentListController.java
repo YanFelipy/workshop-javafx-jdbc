7d6662f6f5ce08844d2a0ef34e4f2812609f1589
YanFelipy22 <yan-felipy@hotmail.com>
2021-05-28 12:28:08 -0300

    Department obj to DepartmentForm

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -45,8 +45,9 @@ public class DepartmentListController implements Initializable {
 
 	@FXML
 	public void onBtNewAction(ActionEvent event) {
+		Department obj = new Department();
 		Stage parentStage = Utils.currentStage(event);
-		createDialogForm("/gui/DepartmentForm.fxml", parentStage);
+		createDialogForm(obj,"/gui/DepartmentForm.fxml", parentStage);
 	}
 
 	public void setDepartmentService(DepartmentService service) {
@@ -61,8 +62,8 @@ public class DepartmentListController implements Initializable {
 
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
-		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("name"));
-
+		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+	
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 
@@ -78,10 +79,15 @@ public class DepartmentListController implements Initializable {
 
 	}
 	
-	private void createDialogForm(String absoluteName, Stage parentStage) {
+	private void createDialogForm(Department obj, String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader	loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+					controller.setDepartment(obj);
+			controller.updateFormData();
+			
 			
 			Stage dialogStage = new Stage();
 			dialogStage.setScene(new Scene(pane));
